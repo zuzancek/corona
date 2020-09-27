74d6480048550d553eb9490979fff21eb2c67b73
zuzancek <zuzana.mucka@gmail.com>
2020-09-27 22:31:22 +0200

    sdfdsfsdf

--- SEIR_2_wave/utils/get_R0.m
+++ SEIR_2_wave/utils/get_R0.m
@@ -5,9 +5,9 @@ r = zeros(N,T);
 for t=1:T
     w = w_vec(t);
     n1 = round(w*N);
-    r1 = gamrnd(g1.r0_scale*g1.mean,1/g1.r0_scale,n1);
+    r1 = gamrnd(g1.r0_scale*g1.mean*g1.alpha,1/g1.r0_scale,n1);
     r(1:n1,t) = r1;
     n2 = N-n1;
-    r2 = power_law(rand(n2),g2.a0,g2.a1,g2.scale);
+    r2 = power_law(rand(n2),g2.a0*g2.alpha,g2.a1*g2.alpha,g2.scale);
     r(n1+1:end,t) = r2;
 end
--- SEIR_2_wave/utils/setparam.m
+++ SEIR_2_wave/utils/setparam.m
@@ -10,8 +10,20 @@ s.T_inf.std = 0.62;
 s.T_inc.mean = 5.1;
 s.Tinc.std = 0.62;
 s.T_overlay = 1.5;
-s.T_rem.mean = s.T_inf.mean+s.T_inc.mean-
+s.T_rem.mean = s.T_inf.mean+s.T_inc.mean-s.T_overlay;
 
+g1.r0_scale = 10^2;
+g1.mean = 0.98;
+g1.alpha = 1;
+s.g1 = g1;
+g2.a0 = g1.mean+1/g1.r0_scale;
+g2.a1 = 14;
+g2.scale = 2.3;
+g2.alpha = 1;
+s.g2 = g2;
+s.w_vec_default = 0.5+zeros(s.T,1);
+
+s.obs_ratio = 1/16;
 
 
 end
