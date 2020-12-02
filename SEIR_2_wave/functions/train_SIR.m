68f9222d08c8559748d5ae5cb4e595b8a8eb296f
zuzancek <zuzana.mucka@gmail.com>
2020-12-02 16:16:45 +0100

    dc

--- SEIR_2_wave/functions/train_SIR.m
+++ SEIR_2_wave/functions/train_SIR.m
@@ -11,82 +11,64 @@ pop_size = s.pop_size;
 init = inputs.init;
 Rt = inputs.Rt;
 N = s.sim_num;
-T_test0 = inputs.T_test;
+% T_test0 = inputs.T_test;
 alpha = inputs.asymp_ratio;
 tau = inputs.obs_ratio;
-lambda = s.lambda;
+% lambda = s.lambda;
 s.share_reas =1;
-T_inf_unobs = get_rv_I_unobs();
+% T_inf_unobs = get_rv_I_unobs();
 
 % init values (observed)
 St = init.S(1);
 Iot = init.Io(1);
-if isfield(init,'Iu') 
-    Iut = init.Iu(1);
-    Iut_next = init.Iu(2);
-elseif isfield (init,'I')
-    Iut = init.I(1)-Iot;
-    Iut_next = init.I(2)-init.Io(2);
-else
-    Iut = Iot*(1-1/inputs.obs_ratio(1));
-    Iut_next = init.Io(2)*(1-1/inputs.obs_ratio(2));    
-end
-if isfield(init,'E')
-    Et = init.E(1);
-else    
-    Et = (Iut_next-Iut.*(1-1./s.T_inf_unobs.mean(1))).*s.T_inc.mean;
-end
 
 % setup
 S_vec = zeros(N,T+1);       S_vec(:,1) = St;
-E_vec = zeros(N,T+1);       E_vec(:,1) = Et;
 Iu_vec = zeros(N,T+1);      Iu_vec(:,1) = Iut;
 Io_vec = zeros(N,T+1);      Io_vec(:,1) = Iot;
 Ia_vec = zeros(N,T+1);      Ia_vec(:,1) = alpha(1)*Iot; 
 Is_vec = zeros(N,T+1);      Is_vec(:,1) = (1-alpha(1))*Iot; 
+dI_in_vec = zeros(N,T+1);     
 R_eff = zeros(N,T+1);       R_eff(:,1) = Rt(:,1);
-dE_in_vec = zeros(N,T+1);   
 st = zeros(T+1,1);
 it = st; iot = st; iut = st; ist = st; iat = st;
 
 %% initialize
-T_lat = get_rv(s.T_lat);
-T_pre = get_rv(s.T_pre);
-T_test = repmat(T_test0',N,1)+repmat(T_pre,1,T);
-T_inf_obs0 = get_rv(s.T_inf_obs0);
-T_inf_obs = T_inf_obs0+T_test;
-T_hosp = get_rv(s.T_hosp);
-gamma_lat = 1./T_lat;
-gamma_unobs = 1./T_inf_unobs;
-gamma_obs = 1./T_inf_obs;
-gamma_obs0 = 1./T_inf_obs0;
-gamma_test = 1./T_test;
-gamma_hosp = 1./T_hosp;
+T_si_vec = get_rv(s.SI);
+gamma = 1./T_si_vec;
+
+% T_pre = get_rv(s.T_pre);
+% T_test = repmat(T_test0',N,1)+repmat(T_pre,1,T);
+% T_inf_obs0 = get_rv(s.T_inf_obs0); 
+% T_inf_obs = T_inf_obs0+T_test;
+% gamma_lat = 1./T_lat;
+% gamma_unobs = 1./T_inf_unobs;
+% gamma_obs = 1./T_inf_obs;
+% gamma_obs0 = 1./T_inf_obs0;
+% gamma_test = 1./T_test;
+% gamma_hosp = 1./T_hosp;
 
 idx = ones(N,1);
 kappa_res = get_kappa_res();
 kappa_mob = get_kappa_mob();
-varsigma_unobs = s.self_isolation_effect*ones(T,1);
-varsigma_obs = s.case_isolation_effect*ones(T,1);
+% varsigma_unobs = s.self_isolation_effect*ones(T,1);
+% varsigma_obs = s.case_isolation_effect*ones(T,1);
+
+alpha = ((s.T_inf_obs.mean-s.T_inf_obs0.mean)+s.T_inf_obs0.mean/s.case_isolation_effect)/s.T_inf_unobs.mean;
 
 for t=1:T
     R_eff(:,t) = Rt(t).*kappa_mob(t).*kappa_res(t);
-    dE_in_vec(:,t) = R_eff(:,t).*S_vec(:,t)/pop_size.*...
-        (varsigma_obs(t).*Io_vec(:,t).*((1-lambda)*gamma_obs(:,t)+lambda*gamma_hosp)...
-         +varsigma_unobs(t).*Iu_vec(:,t).*gamma_unobs(:,t));
-    S_vec(:,t+1) = S_vec(:,t)-dE_in_vec(:,t);     
-    E_vec(:,t+1) = E_vec(:,t).*(1-gamma_lat)+dE_in_vec(:,t);
-    Iu_vec(:,t+1) = Iu_vec(:,t)+gamma_lat.*E_vec(:,t)-tau(t).*Iu_vec(:,t).*gamma_test(:,t)...
-        -(1-tau(t)).*Iu_vec(:,t).*gamma_unobs(:,t);
-    Io_vec(:,t+1) = Io_vec(:,t)+tau(t).*Iu_vec(:,t).*gamma_test(:,t)...
-        -(lambda.*alpha(t).*gamma_hosp+(1-lambda.*alpha(t)).*gamma_obs0).*Io_vec(:,t);
+    dI_in_vec(:,t) = R_eff(:,t).*S_vec(:,t)/pop_size.*...
+        (alpha.*Io_vec(:,t)+Iu_vec(:,t)).*gamma;
+    S_vec(:,t+1) = S_vec(:,t)-dI_in_vec(:,t);   
+    Io_vec(:,t+1) = Io_vec(:,t)+tau(t).*dI_in_vec(:,t)-gamma.*Io_vec(:,t);
+    Iu_vec(:,t+1) = Iu_vec(:,t)+(1-tau(t)).*dI_in_vec(:,t)-gamma.*Iu_vec(:,t);
     Ia_vec(:,t) = alpha(t).*Io_vec(:,t); Is_vec(:,t) = Io_vec(:,t)-Ia_vec(:,t);
-    idx = idx & Io_vec(:,t+1)>=0 & Iu_vec(:,t+1)>=0 & E_vec(:,t+1)>=0;
+    idx = idx & Io_vec(:,t+1)>=0 & Iu_vec(:,t+1)>=0;
 end
 
 idx = find(idx>0);
 S_vec = S_vec(idx,:);
-% E_vec = E_vec(idx,:);
 Iu_vec = Iu_vec(idx,:);
 Io_vec = Io_vec(idx,:);
 Ia_vec = Ia_vec(idx,:);
@@ -155,26 +137,6 @@ res_quant.St = get_quant(St_vec);
         end
     end
 
-    function [x] = get_rv_I_unobs()
-        L = size(s.T_inf_obs.mean,2);
-        % L = size(T_inf_asymp.mean,2);
-        % sh0 = T_inf_asymp.mean.*repmat(share_asymp,N,1)+repmat((1-share_asymp),N,1)*T_inf_symp.mean;
-        n0 = floor(N*s.share_reas);
-        sh0 = (s.T_inf_obs.mean).*ones(N,1);
-        x0 = gamrnd(sh0.*s.T_inf_obs.std.^2,1/s.T_inf_obs.std^2);
-        x0 = repmat(x0(1:n0,:),1,T);
-        s.T_inf_unobs.d1.mean = sh0; s.T_inf_unobs.d1.std = s.T_inf_asymp.std; 
-        n1 = N-n0;
-        if n1>0
-            x1 = rand(n1,T); a0 = sh0; a1 = 10; sc = 2.3;
-            x1 = (power_law(x1,a0(n0+1:end,:),a1,sc));
-            s.T_inf_unobs.d2.a0 = a0; s.T_inf_unobs.d2.a1 = a1;s.T_inf_unobs.d2.k = sc;
-            s.T_inf_unobs.s0 = s.share_reas;
-            x = [x0;x1];
-        else
-            x = x0;
-        end
-        s.T_inf_unobs.mean = mean(x); s.T_inf_unobs.std = std(x);
-    end
+    
 
 end
