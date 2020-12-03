477699eb75df9a01debc6681b949f677ad0dbc00
zuzancek <zuzana.mucka@gmail.com>
2020-12-03 10:45:52 +0100

    werew

--- SEIR_2_wave/b_01_train_model.m
+++ SEIR_2_wave/b_01_train_model.m
@@ -56,9 +56,13 @@ inputs.restrictions = restrictions;
 inputs.Rt = data_Rt.Rt_rnd;%(:,train_from-2*data.s.SI.mean:train_to-2*data.s.SI.mean);
 
 %% model training
+s.quant = 0.25:0.025:0.75;
 [res_mean,res_quant] = train_SIR(time_interval,inputs,data.s);
 
-% %% display results
+%% display results
+plot_fanchart(res_quant.It,s,dt,train_from,train_to,train_from,'Total active infections (SIR, PCR only)',true,8);
+plot_fanchart(res_quant.Iot,s,dt,train_from,train_to,train_from,'Observed active infections (SIR, PCR only)',true,8);
+
 % tm0 = dd(2020,9,15);
 % tm1 = t1;
 % % tune theta
--- SEIR_2_wave/utils/plot_fanchart.m
+++ SEIR_2_wave/utils/plot_fanchart.m
@@ -1,4 +1,4 @@
-function [] = plot_fanchart(q_mat,s,dt,disp_from,disp_to,t0,fname,maketit)
+function [] = plot_fanchart(q_mat,s,dt,disp_from,disp_to,t0,fname,maketit,varargin)
 
 f = figure('Name',fname);
 fanChart(1:size(q_mat,2), q_mat', q_mat(s.quant_idx_central,:), s.quant,...
@@ -14,10 +14,16 @@ ax.YAxis.Color = s.color_grid;
 f.Color = s.color_bkg;
 p0 = dt+(disp_from-t0+1);
 p1 = disp_to-t0+1;
+lab = {'marec','april','maj','jun','jul','august','september','oktober','november','december'};
 mt = [0 31 30 31 30 31 31 30 31 30 31];
-mtt = cumsum(mt);
+if isempty(varargin)
+    lab_idx = 1;
+else
+    lab_idx = varargin{1};
+end
+mtt = cumsum(mt(lab_idx:end));
 xticks(mtt)
-xticklabels({'marec','april','maj','jun','jul','august','september','oktober','november','december'});
+xticklabels(lab(lab_idx:end));
 xlim([p0 p1]);
 if maketit
     title(fname,'Color',s.color_grid);
