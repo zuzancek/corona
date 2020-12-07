ff5454a10762fae181941bb5dfaa08abe6f08e90
zuzancek <zuzana.mucka@gmail.com>
2020-12-07 22:16:24 +0100

    wrrew

--- SEIR_2_wave/b_01_train_model.m
+++ SEIR_2_wave/b_01_train_model.m
@@ -65,14 +65,14 @@ s.quant = 0.25:0.025:0.75;
 
 %% display results
 % infectious in distributions
-ydata.data = init.I; ydata.col = 'r'; ydata.leg = 'Data-implied';
+ydata.data = init.It; ydata.col = 'r'; ydata.leg = 'Data-implied';
 plot_fanchart(res_quant.It,s,dt,train_from,train_to,train_from,'Total active infections (SIR, PCR only)',true,8,ydata);
-ydata.data = init.Io; ydata.col = 'r'; ydata.leg = 'Data-implied';
+ydata.data = init.Iot; ydata.col = 'r'; ydata.leg = 'Data-implied';
 plot_fanchart(res_quant.Iot,s,dt,train_from,train_to,train_from,'Observed active infections (SIR, PCR only)',true,8,ydata);
 
 % Data vs model-implied infectious
 figure('Name','Data vs. model-implied observed infectious');
-plot(init.Io,'linewidth',1);hold on;
+plot(init.Iot,'linewidth',1);hold on;
 plot(res_mean.Iot,'linewidth',1);hold on;
 plot(res_quant.Iot(ceil(length(s.quant)/2),:),'linewidth',1);hold on;
 grid on;
