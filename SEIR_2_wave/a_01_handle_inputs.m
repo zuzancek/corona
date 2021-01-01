a9691c80550024dbaf3198198ed52e62516e1784
zuzancek <zuzana.mucka@gmail.com>
2021-01-01 16:05:44 +0100

    dd

--- SEIR_2_wave/a_01_handle_inputs.m
+++ SEIR_2_wave/a_01_handle_inputs.m
@@ -65,12 +65,9 @@ deaths_withCovid_smooth = smooth_series(deaths_withCovid,s.smooth_width_hosp,s.s
 
 %% calculations
 % asymptomatic share
-final.date = t1; final.value = 13.5;
-initial.date = t0; initial.value = 25; 
-breakpoint.date = dd(2020,10,30);breakpoint.value = 29.5;
 try
-    [asymp_ratio,asymp_ratio_smooth] = process_as('data/asympt_share.xlsx',dd(2020,3,13),dd(2020,10,30),...
-        s,initial,final,breakpoint,dd(2020,11,30));
+    [asymp_ratio,asymp_ratio_smooth] = process_xls('data/asympt_share.xlsx',dd(2020,3,13),dd(2020,10,30),...
+        dd(2020,3,13),t1,s,[],[]);
 catch err
     pr = load('inputs.mat','dI_inflow_pcr','dI_inflow_pcr_smooth','dI_inflow_real','dI_inflow_smooth','dI_inflow_real_smooth',...
         'pos_test_ratio_smooth','obs_ratio','asymp_ratio_smooth',...
