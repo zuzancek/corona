e9ec3b97d1b6a3e522dabbb2be76591bde5734b2
zuzancek <zuzana.mucka@gmail.com>
2021-01-17 15:01:15 +0100

    wefew

--- SEIR_2_wave/utils/DHIXt.m
+++ SEIR_2_wave/utils/DHIXt.m
@@ -40,20 +40,25 @@ k_death = 30; x_death = 1:k_death;
 p_T_death = pdf('Exponential',repmat(x_death,length(varsigma),1),repmat(T_death,1,k_death));
 % hospitalizations
 % old-young share in hospitals
-zeta0 = (1-varsigma)./varsigma.*omega_y./omega_o; zeta = zeta0./(1+zeta0);
+zeta0 = (varsigma)./(1-varsigma).*omega_y./omega_o; zeta = zeta0./(1+zeta0);
 % recovery     
 % T_rec_y = s.T_rec_y;    T_rec_o = s.T_rec_o;            T_rec = zeta.*T_rec_o+(1-zeta).*T_rec_y;
-T_rec_y = 6.566; T_rec_o = 9.527; T_rec_std = 0.61; T_rec = zeta.*T_rec_o+(1-zeta).*T_rec_y;
+T_rec_y = 9.566; T_rec_o = 12.527; T_rec_std = 0.61; T_rec = zeta.*T_rec_o+(1-zeta).*T_rec_y;
 k_rec = 30; x_rec = 1:k_rec;   T_rec_shape = T_rec*T_rec_std^2; T_rec_scale = 1/T_rec_std^2;
 p_T_rec = pdf('Gamma',repmat(x_rec,length(zeta),1),repmat(T_rec_shape,1,k_rec),repmat(T_rec_scale,length(zeta),k_rec));
 % hospital admission
 lambda_y = 2.32/100;    lambda_o = 31.86/100;         
-theta0 = (1-zeta)./zeta.*lambda_y./lambda_o; theta = theta0./(1+theta0);
+theta0 = zeta0.*lambda_y./lambda_o; theta = theta0./(1+theta0);
 lambda = theta.*lambda_o+(1-theta).*lambda_y;
 T_hosp_y = 5.63;        T_hosp_o = 1.33;              T_hosp = T_hosp_o.*theta+T_hosp_y*(1-theta);
 k_hosp = 20; x_hosp = 1:k_hosp;                   
 p_T_hosp = pdf('Exponential',repmat(x_hosp,length(varsigma),1),repmat(T_hosp,1,k_hosp));
-
+% infections
+% recovery from sickness
+T_sick_y = s.SI.mean-1; T_sick_o = s.SI.mean+1; T_sick_std = s.SI.mean; T_sick = theta.*T_sick_o+(1-theta).*T_sick_y;
+k_sick = 20; x_sick = 1:k_sick;   T_sick_shape = T_sick*T_sick_std^2; T_sick_scale = 1/T_sick_std^2;
+eta = 1-lambda;
+p_T_sick = pdf('Gamma',repmat(x_sick,length(zeta),1),repmat(T_sick_shape,1,k_sick),repmat(T_sick_scale,length(zeta),k_sick));
 
 % T_death_y = s.T_death_y;                             alpha_hdy = omega_y/T_death_y; s.alpha_hdy = alpha_hdy;
 % T_death_o = s.T_death_o;                             alpha_hdo = omega_o/T_death_o; s.alpha_hdo = alpha_hdo;
@@ -79,14 +84,16 @@ H0 = method_data(h.Hospitalizations(dateFrom-k_death:dateTo));
 H01 = method_data(h.Hospitalizations(dateFrom-k_rec:dateTo));
 
 HD = method_data(D(2:end)-D(1:end-1));  HD = [HD(1);HD(:)];
-hd = get_wa(p_T_death(k_death+1:end,:),H,omega(k_death+1:end),k_death);
+hd = get_wa(p_T_death,H,omega,k_death);
 gamma_hd =  extend(HD(k_death+1:end)./hd,k_death);
 gamma_hd = method_params(gamma_hd);
-alpha_rec = 1-omega.*gamma_hd; 
+alpha = 1-omega.*gamma_hd; 
 % HR = alpha_rec./T_rec.*H;
-HR = extend(get_wa(p_T_rec(k_rec+1:end,:),H,alpha_rec(k_rec+1:end),k_rec),k_rec);
+HR = extend(get_wa(p_T_rec,H,alpha,k_rec),k_rec);
 IH = H(2:end)-H(1:end-1)+HR(2:end)+HD(2:end);IH = [IH(1);IH(:)];
-
+I = get_wa_inv(p_T_hosp(k_hosp+1:end,:),IH,lambda(k_hosp+1:end),k_hosp);
+IR = extend(get_wa(p_T_sick,I,eta,k_sick),k_sick);
+X = I(2:end)-I(1:end-1)+IR(2:end)+IH(2:end);X = [X(1);X(:)];
 % H_y_H_o = adjust_series(alpha_hdo./alpha_hdy.*H_D_y./H_D_o);
 % H_y = method_params(H_y_H_o./(H_y_H_o+1)).*H;
 % H_o = H-H_y;
@@ -208,15 +215,18 @@ p.varsigma = varsigma;
 
     function [x] = get_wa(weight,Z,alpha,idxFrom)
         sz = size(weight);
+        weight = weight(idxFrom+1:end,:);
+        alpha = alpha(idxFrom+1:end,:);
         k = sz(2);k0 = sz(1);
         %k = length(weight);
         t = length(Z)-idxFrom;
+        phi = alpha./(1:k);
         if k0==1
-            W = repmat(weight,t,1);
-            A = repmat(alpha./(1:k),t,1);
+            W = repmat(weight(end:-1:1),t,1);
+            A = repmat(phi(end:-1:1),t,1);
         else
-            W = weight;
-            A = alpha./(1:k);
+            W = weight(:,end:-1:1);
+            A = phi(:,end:-1:1);
         end
         J = repmat(1:k,t,1)+repmat((0:t-1)',1,k);
         L = 0*(k-1)+repmat((1:t)',1,k);
@@ -226,32 +236,67 @@ p.varsigma = varsigma;
         x = (Weight_mat.*Alpha_mat)*Z(end-t-k+1:end-1);
     end
 
-    function [x] = get_wa_inv(weight,Z,phi,idxFrom)
+    function [x] = get_wa_inv(weight,z,x0,alpha,idxFrom)
         sz = size(weight);
+        weight = weight(idxFrom+1:end,:);
+        alpha = alpha(idxFrom+1:end,:);
         k = sz(2);k0 = sz(1);
-        t = length(Z)-idxFrom;
-        aalpha = phi./(1:k);
+        t = length(z)-idxFrom;
+        phi = alpha./(1:k);
         if k0==1
-            W = repmat(weight,t,1);
-            A = repmat(aalpha,t,1);
+            W = repmat(weight(k:-1:1),t,1);
+            A = repmat(phi(k:-1:1),t,1);
         else
-            W = weight;
-            A = aalpha;
+            W = weight(:,(k:-1:1));
+            A = phi(:,(k:-1:1));
         end        
         J = repmat(1:k,t,1)+repmat((0:t-1)',1,k);
         L = (k-1)+repmat((1:t)',1,k);
-        U0 = tril(repmat(1:k-1,k-1,1)); 
-        % U0(U0==0) = k+1; 
-        J0 = repmat(1:k-1,k-1,1); J0 = J0(U0~=0);
-        L0 = repmat((1:k-1)',1,k-1); L0 = L0(U0~=0);
-        w = weight(k,:); w(end+1) = 0; a = aalpha(k,:);
-        W0 = w(U0(U0~=0))'; 
-        A0 = a(U0(U0~=0))';
-        Weight_mat = sparse([L(:);L0(:)],[J(:);J0(:)],[W(:);W0(:)]);
-        Alpha_mat = sparse([L(:);L0(:)],[J(:);J0(:)],[A(:);A0(:)]);
+%         U0 = tril(repmat(1:k-1,k-1,1)); 
+%         % U0(U0==0) = k+1; 
+%         J0 = repmat(1:k-1,k-1,1); J0 = J0(U0~=0);
+%         L0 = repmat((1:k-1)',1,k-1); L0 = L0(U0~=0);
+%         w = weight(k,:); w(end+1) = 0; a = phi(k,:);
+%         W0 = w(U0(U0~=0))'; 
+%         A0 = a(U0(U0~=0))';
+        Weight_mat = sparse(L(:),J(:),W(:));
+        Alpha_mat = sparse(L(:),J(:),A(:));
+        x = zeros(t+k-1,1);x(1:k-1) = x0;
         Weight_mat = Weight_mat./sum(Weight_mat,2);
-        x = (Weight_mat.*Alpha_mat)\Z(end-t-k+1:end-1); 
+        function [d] = solve_lineqn(xx0)
+            d=(Weight_mat.*Alpha_mat)*xx0 - z(end-t+1:end);
+        end
+        x = fsolve(@solve_lineqn,x);
     end
+
+%     function [x] = get_wa_inv(weight,Z,alpha,idxFrom)
+%         sz = size(weight);
+%         weight = weight(idxFrom+1:end,:);
+%         alpha = alpha(idxFrom+1:end,:);
+%         k = sz(2);k0 = sz(1);
+%         t = length(Z)-idxFrom;
+%         phi = alpha./(1:k);
+%         if k0==1
+%             W = repmat(weight(k:-1:1),t,1);
+%             A = repmat(phi(k:-1:1),t,1);
+%         else
+%             W = weight(:,(k:-1:1));
+%             A = phi(:,(k:-1:1));
+%         end        
+%         J = repmat(1:k,t,1)+repmat((0:t-1)',1,k);
+%         L = (k-1)+repmat((1:t)',1,k);
+%         U0 = tril(repmat(1:k-1,k-1,1)); 
+%         % U0(U0==0) = k+1; 
+%         J0 = repmat(1:k-1,k-1,1); J0 = J0(U0~=0);
+%         L0 = repmat((1:k-1)',1,k-1); L0 = L0(U0~=0);
+%         w = weight(k,:); w(end+1) = 0; a = phi(k,:);
+%         W0 = w(U0(U0~=0))'; 
+%         A0 = a(U0(U0~=0))';
+%         Weight_mat = sparse([L(:);L0(:)],[J(:);J0(:)],[W(:);W0(:)]);
+%         Alpha_mat = sparse([L(:);L0(:)],[J(:);J0(:)],[A(:);A0(:)]);
+%         Weight_mat = Weight_mat./sum(Weight_mat,2);
+%         x = (Weight_mat.*Alpha_mat)\Z(end-t-k+1:end-1); 
+%     end
 
     function [y] = extend(x,t0)
         xlen = length(x);
