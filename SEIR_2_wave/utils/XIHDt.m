c19f6626667c6b186cdc7e5fd7477ad897e88b34
zuzancek <zuzana.mucka@gmail.com>
2021-01-20 15:43:00 +0100

    sdfsd

--- SEIR_2_wave/utils/XIHDt.m
+++ SEIR_2_wave/utils/XIHDt.m
@@ -13,14 +13,6 @@ catch err
 end
 varsigma = method_params(init.varsigma); varsigma = varsigma(dateFrom:dateTo);
      
-alpha_ihy = s.eta_y/s.T_hosp_y;        
-alpha_iho = s.eta_o/s.T_hosp_o;  
-alpha_iry = (1-s.eta_y)./p.T_sick_y; 
-alpha_iro = (1-s.eta_o)./p.T_sick_o;
-alpha_hdy = p.alpha_hdy; %s.omega_y/s.T_death_y;
-alpha_hdo = p.alpha_hdo; % s.omega_o/s.T_death_o;
-alpha_hry = p.alpha_hry; % (1-s.omega_y)./p.T_rec_y; 
-alpha_hro = p.alpha_hro; %(1-s.omega_o)./p.T_rec_o; 
 eta_y = s.eta_y;                   
 eta_o = s.eta_o;           
 
@@ -45,15 +37,45 @@ I_y = zeros(T,1); I_y(1) = I0(1)-I_o(1);
 d_I_H_o = zeros(T,1);  d_I_H_y = d_I_H_o; d_I_R_o = d_I_H_o; d_I_R_y = d_I_H_o;
 d_H_D_o = d_I_H_o; d_H_D_y = d_I_H_o; d_H_R_o = d_I_H_o; d_H_R_y = d_I_H_o;
 
+startFrom = max(s.k_hosp,s.k_sick,s.k_death,s.k_rec)+1;
+T_total = T+startFrom;
+% parameters
+% hospital admission
+k_hosp = s.k_hosp;      x_hosp = (1:k_hosp)'; 
+T_hosp_o = s.T_hosp_o;  p_T_hosp_o = pdf(s.T_hosp_pdf_type,x_hosp,1./T_hosp_o+zeros(k_hosp,1));
+T_hosp_y = s.T_hosp_y;  p_T_hosp_y = pdf(s.T_hosp_pdf_type,x_hosp,1./T_hosp_y+zeros(k_hosp,1));
+eta_o = s.eta_o;        alpha_iho = eta_o./x_hosp.*p_T_hosp_o;
+eta_y = s.eta_y;        alpha_ihy = eta_y./x_hosp.*p_T_hosp_y;
+% recovery from sickness at home
+k_sick = s.k_sick;      x_sick = (1:k_sick)';       x_sick_mat = repmat(x_sick,T_total,1); T_sick_std2 = s.T_sick_std^2;
+T_sick_o = s.T_sick_o-p.T_delay-p.Test_to_result;   T_sick_o_shape = T_sick_o*T_sick_std2; T_sick_scale = 1/T_sick_std2;
+p_T_sick_o = pdf(s.T_sick_pdf_type,x_sick_mat,repmat(T_sick_o_shape,1,k_sick),repmat(T_sick_scale,1,k_sick));
+T_sick_y = s.T_sick_y-p.T_delay-p.Test_to_result;   T_sick_y_shape = T_sick_y*T_sick_std2; 
+p_T_sick_y = pdf(s.T_sick_pdf_type,x_sick_mat,repmat(T_sick_y_shape,1,k_sick),repmat(T_sick_scale,1,k_sick));
+alpha_iro = (1-eta_o)./x_sick_mat.*p_T_sick_o;
+alpha_iry = (1-eta_y)./x_sick_mat.*p_T_sick_y;
+% death at hospital
+k_death = s.k_death;        x_death = (1:k_death)';
+T_death_o = s.T_death_o;    p_T_death_o = pdf(s.T_death_pdf_type,x_death,1./T_death_o+zeros(k_death,1));
+T_death_y = s.T_death_y;    p_T_death_y = pdf(s.T_death_pdf_type,x_death,1./T_death_y+zeros(k_hosp,1));
+omega_o = s.omega_o;        alpha_hdo = omega_o./x_death.*p_T_death_o;
+omega_y = s.omega_y;        alpha_hdy = omega_y./x_death.*p_T_death_y;
+% recovery at hospital
+k_rec = s.k_rec;            x_rec = (1:k_rec)';                     T_rec_std2 = s.T_rec_std^2; T_rec_scale = 1/T_rec_std2+zeros(k_sick,1);
+T_rec_o = s.T_rec_o;        T_rec_o_shape = T_rec_o*T_sick_std2;    p_T_rec_o = pdf(s.T_rec_pdf_type,x_rec,T_rec_o_shape+zeros(k_sick,1),T_rec_scale);
+T_rec_y = s.T_rec_y;        T_rec_y_shape = T_rec_y*T_sick_std2;    p_T_rec_y = pdf(s.T_rec_pdf_type,x_rec,T_rec_y_shape+zeros(k_sick,1),T_rec_scale);
+alpha_hro = (1-omega_o)./x_rec.*p_T_rec_o;
+alpha_hry = (1-omega_y)./x_rec.*p_T_rec_y;
+
 % calculation
-for t=1:T-1
-    d_I_H_o(t) = alpha_iho.*I_o(t);     d_I_R_o(t) = alpha_iro(t).*I_o(t);
+for t=startFrom:T_total-1 
+    d_I_H_o(t) = dot(alpha_iho,I_o(t-k_hosp+1:t));     d_I_R_o(t) = dot(alpha_iro(t,:),I_o(t-k_sick+1:t));
     I_o(t+1) = I_o(t)+X_o(t)-d_I_H_o(t)-d_I_R_o(t);
-    d_I_H_y(t) = alpha_ihy.*I_y(t);  d_I_R_y(t) = alpha_iry(t).*I_y(t);
+    d_I_H_y(t) = dot(alpha_ihy,I_y(t-k_hosp+1:t));     d_I_R_y(t) = dot(alpha_iry(t,:),I_y(t-k_sick+1:t));
     I_y(t+1) = I_y(t)+X_y(t)-d_I_H_y(t)-d_I_R_y(t);
-    d_H_D_o(t) = alpha_hdo.*H_o(t);  d_H_R_o(t) = alpha_hro.*H_o(t);
+    d_H_D_o(t) = dot(alpha_hdo,H_o(t-k_death+1:t));    d_H_R_o(t) = dot(alpha_hro,H_o(t-k_rec+1:t));
     H_o(t+1) = H_o(t)+d_I_H_o(t)-d_H_D_o(t)-d_H_R_o(t);
-    d_H_D_y(t) = alpha_hdy.*H_y(t);  d_H_R_y(t) = alpha_hry.*H_y(t);
+    d_H_D_y(t) = dot(alpha_hdy,H_y(t-k_death+1:t));    d_H_R_y(t) = dot(alpha_hry,H_y(t-k_rec+1:t));
     H_y(t+1) = H_y(t)+d_I_H_y(t)-d_H_D_y(t)-d_H_R_y(t);
     D_o(t+1) = D_o(t)+d_H_D_o(t);
     D_y(t+1) = D_y(t)+d_H_D_y(t);
