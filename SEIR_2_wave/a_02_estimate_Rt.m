8afeeb5aad66b80ed2de2aa03727761f65a694d5
zuzancek <zuzana.mucka@gmail.com>
2021-01-22 01:34:06 +0100

    dsds

--- SEIR_2_wave/a_02_estimate_Rt.m
+++ SEIR_2_wave/a_02_estimate_Rt.m
@@ -44,12 +44,11 @@ s = setparam();
 % reported data, PCR only, testing is optimal (sstate observ.ratio)
 xx = resize(cases_data.cases_pcr_smooth,t0:t2);
 xx(d0:d1) = NaN; xx = interp(xx,t0:t2);
-cases_data.cases_pcr_smooth = xx;
+cases_data.cases_pcr_smooth = smooth_series(xx);
 inputs_fnc.z = double(resize(cases_data.cases_pcr_smooth,t0:t2));
 inputs_fnc.I0 = cases_data.I0;
 inputs_fnc.H0 = 0;
 inputs_fnc.D0 = 0;
-%
 inputs_fnc.obs_ratio = [];
 inputs_fnc.old_ratio = [];
 inputs_fnc.death_ratio = []; 
@@ -57,10 +56,10 @@ inputs_fnc.asymp_ratio = [];
 [Rt_pcr,q_mat_pcr,Yt_pcr,Rt_last_pcr,Rt_dist_pcr,Rt_rnd_pcr]  = model_fnc(inputs_fnc,s,true,true,false);
 % real (implied) data
 z0 = resize(cases_data.cases_pcr_smooth,t0:t2);
-z0(t1:t2) = cases_data.cases_pcr_implied;
+z0(t1+3:t2) = cases_data.cases_pcr_implied(t1+3:t2);
 z0(d0:d1) = NaN; z0 = interp(z0,t0:t2);
-cases_data.cases_pcr_implied = z0;
-inputs_fnc.z = double(resize(z0,t0:t2));
+cases_data.cases_pcr_implied = smooth_series(z0);
+inputs_fnc.z = double(resize(cases_data.cases_pcr_implied,t0:t2));
 inputs_fnc.obs_ratio = [];
 inputs_fnc.old_ratio = params.old_ratio;
 inputs_fnc.death_ratio = params.death_ratio;
@@ -79,8 +78,8 @@ legend({'Implied by hospitals','PCR reported','AG+PCR reported'});
 % 1./ reproduction number
 figure('Name','Effective reproduction number, means');
 nn = length(Rt_pcr);
-Rt_smooth_series_pcr = tseries(t0+1:t2,Rt_pcr);
-Rt_smooth_series_real = tseries(t0+1:t2,Rt_real);
+Rt_smooth_series_pcr = tseries(t0:t2,Rt_pcr);
+Rt_smooth_series_real = tseries(t0:t2,Rt_real);
 plot(resize(Rt_smooth_series_pcr,disp_from:t2),'linewidth',2);hold on;
 plot(resize(Rt_smooth_series_real,disp_from:t2-tshift),'linewidth',2);hold on;
 title('Effective reproduction number (smooth inputs)');
@@ -93,8 +92,8 @@ plot_fanchart(q_mat_pcr,s,dt,disp_from,disp_to,t0,'Effective reproduction number
 % 2./ situation in hospitals
 figure('Name','Hospitals & Deaths, means');
 subplot(2,1,1)
-Dt_pcr = tseries(t0+1:t2,Yt_pcr.Dt(1:nn));
-Dt_real = tseries(t0+1:t2,Yt_real.Dt(1:nn));
+Dt_pcr = tseries(t0:t2,Yt_pcr.Dt(1:nn));
+Dt_real = tseries(t0:t2,Yt_real.Dt(1:nn));
 % Dt_total = tseries(t0+1:t2,Yt_total.Dt(1:nn));
 kappa_d = resize(Dt_real,disp_from:t2)./resize(hosp_data.D_smooth,disp_from:t2);
 Dt_real = resize(Dt_real,disp_from:t2)./kappa_d;
@@ -108,8 +107,8 @@ legend({'reported data, PCR only',...
     'implied data'});
 grid on;
 subplot(2,1,2)
-Ht_pcr = tseries(t0+1:t2,Yt_pcr.Ht(1:nn));
-Ht_real = tseries(t0+1:t2,Yt_real.Ht(1:nn));
+Ht_pcr = tseries(t0:t2,Yt_pcr.Ht(1:nn));
+Ht_real = tseries(t0:t2,Yt_real.Ht(1:nn));
 kappa_h = resize(Ht_real,disp_from:t2)./resize(hosp_data.H_smooth,disp_from:t2);
 Ht_real = resize(Ht_real,disp_from:t2)./kappa_h;
 Ht_pcr = resize(Ht_pcr,disp_from:t2)./kappa_h;
