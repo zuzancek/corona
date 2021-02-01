62c774efe85e30ca292ea3d0f81f46cef4fb9a11
zuzancek <zuzana.mucka@gmail.com>
2021-02-01 01:00:34 +0100

    erg

--- SEIR_2_wave/utils/get_prob_dist.m
+++ SEIR_2_wave/utils/get_prob_dist.m
@@ -3,12 +3,14 @@ function [opt_fit,kernel_fit,pdf_grid,cdf_grid,rv_grid] = get_prob_dist(N_rand,t
 ip = inputParser;
 addParamValue(ip, 'do_plot', true, @islogical);%#ok<*NVREPL>
 addParamValue(ip, 'do_fitdist', true, @islogical);%#ok<*NVREPL>
+addParamValue(ip, 'title', '', @ischar);%#ok<*NVREPL>
 addParamValue(ip, 'dist_list', {'Kernel','Weibull','Gamma','Burr','GeneralizedExtremeValue','Lognormal','Loglogistic'}, @iscell);%#ok<*NVREPL>
 parse(ip, varargin{:});
 results = ip.Results;
 do_plot = results.do_plot;
 do_fitdist = results.do_fitdist;
 dist_list = results.dist_list;
+tit = results.title;
 
 rnd_val = rand(N_rand,1);
 min_val = min(rnd_val); max_val =  max(rnd_val);
@@ -22,7 +24,7 @@ kernel_fit = fitdist(rv_grid,'Kernel');
 opt_fit = kernel_fit;
 
 if do_plot
-    figure;
+    figure('Name',strcat('Empirical Distribution (',tit,')'));
     [f,x]=ecdf(rv_grid); x(1)=0; 
     subplot(3,1,1); plot(x,f);grid on;title('CDF(x)');
     [f,x]=ecdf(rv_grid,'function','cumulative hazard'); x(1)=0;
@@ -45,13 +47,14 @@ if do_fitdist
         d{i}.mean = mean(d{i}.obj);
         d{i}.median = median(d{i}.obj);
         d{i}.std = std(d{i}.obj);
+        d{i}.time_grid = time_grid;
         fprintf('%s error %2.4f\n',d{i}.type,d{i}.diff);
     end
     [~,idx] = sort(dist);
     idx = idx(2);
     opt_fit = d{idx};
     if do_plot
-        figure;
+        figure('Name',strcat('Fitting Distribution (',tit,')'));
         col = [1 1 1];
         histogram(rv_grid',length(time_grid),'Normalization','probability','binwidth',1,...
             'FaceColor',0.65*col,'EdgeColor',0.5*col,'FaceAlpha',0.5,'EdgeAlpha',0.5); hold on;
