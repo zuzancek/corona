20f00f17b2c461c29f30e1b0b01d4fc5e0948887
zuzancek <zuzana.mucka@gmail.com>
2021-02-01 21:30:57 +0100

    asds

--- SEIR_2_wave/a_00_run_statistics.m
+++ SEIR_2_wave/a_00_run_statistics.m
@@ -15,30 +15,36 @@ s_width = 10; s_type = 5; s_ends = 1;
 
 %% calculations
 % 1. Hospital admission (time to, probability)
-s.alpha_h_y = tb.P_H_Y(end);
-s.alpha_h_o = tb.P_H_O(end);
-cdf_h_y = tb.P_H_Y./s.alpha_h_y;
+alpha_h_y = tb.P_H_Y(end);
+alpha_h_o = tb.P_H_O(end);
+cdf_h_y = tb.P_H_Y./alpha_h_y;
 pdf_h_y = cdf_h_y(2:end)-cdf_h_y(1:end-1);  pdf_h_y = [pdf_h_y;0]/sum(pdf_h_y);
 pdf_h_y_s = smooth_series(pdf_h_y);pdf_h_y_s = pdf_h_y_s/sum(pdf_h_y_s);
-cdf_h_o = tb.P_H_O./s.alpha_h_o;
+cdf_h_o = tb.P_H_O./alpha_h_o;
 pdf_h_o = cdf_h_o(2:end)-cdf_h_o(1:end-1);  pdf_h_o = [pdf_h_o;0]/sum(pdf_h_o);
 pdf_h_o_s = smooth_series(pdf_h_o);pdf_h_o_s = pdf_h_o_s/sum(pdf_h_o_s);
 s.opt_fit_h_y = get_prob_dist(N_rand,x,pdf_h_y_s,'do_plot',true,'title','PHY');
+s.opt_fit_h_y.alpha = alpha_h_y;
 s.opt_fit_h_o = get_prob_dist(N_rand,x,pdf_h_o_s,'do_plot',true,'title','PHO');
+s.opt_fit_h_o.alpha = alpha_h_o;
 % 2. Death (time to, probability ... during hospital stay)
-s.alpha_d_y = tb.P_D_Y(end);
-s.alpha_d_o = tb.P_D_O(end);  
-[pdf_d_y_s,cdf_d_y_s] = apply_censoring(tb.P_D_Y./s.alpha_d_y,25);
-[pdf_d_o_s,cdf_d_o_s] = apply_censoring(tb.P_D_O./s.alpha_d_o,25);
+alpha_d_y = tb.P_D_Y(end);
+alpha_d_o = tb.P_D_O(end);  
+[pdf_d_y_s,cdf_d_y_s] = apply_censoring(tb.P_D_Y./alpha_d_y,25);
+[pdf_d_o_s,cdf_d_o_s] = apply_censoring(tb.P_D_O./alpha_d_o,25);
 s.opt_fit_d_y = get_prob_dist(N_rand,x,pdf_d_y_s,'do_plot',true,'title','PDY');
+s.opt_fit_d_y.alpha = alpha_d_y;
 s.opt_fit_d_o = get_prob_dist(N_rand,x,pdf_d_o_s,'do_plot',true,'title','PDO');
+s.opt_fit_d_o.alpha = alpha_d_o;
 % 3. Recovery (time to, probability ... during hospital stay)
-s.alpha_r_y = tb.P_R_Y(end);
-s.alpha_r_o = tb.P_R_O(end);  
-[pdf_r_y_s,cdf_r_y_s] = apply_censoring(tb.P_R_Y./s.alpha_r_y,25);
-[pdf_r_o_s,cdf_r_o_s] = apply_censoring(tb.P_R_O./s.alpha_r_o,25);
+alpha_r_y = tb.P_R_Y(end);
+alpha_r_o = tb.P_R_O(end);  
+[pdf_r_y_s,cdf_r_y_s] = apply_censoring(tb.P_R_Y./alpha_r_y,25);
+[pdf_r_o_s,cdf_r_o_s] = apply_censoring(tb.P_R_O./alpha_r_o,25);
 s.opt_fit_r_y = get_prob_dist(N_rand,x,pdf_r_y_s,'do_plot',true,'title','PRY');
+s.opt_fit_r_y.alpha = alpha_r_y;
 s.opt_fit_r_o = get_prob_dist(N_rand,x,pdf_r_o_s,'do_plot',true,'title','PRO');
+s.opt_fit_r_o.alpha = alpha_r_o;
 
 %% saving stuff
 save('results/optimal_fit.mat','s');
