45470ab88bd520177aa8459d7bbe93dedc2ef1f1
zuzancek <zuzana.mucka@gmail.com>
2021-02-02 01:07:18 +0100

    ';l;'l

--- SEIR_2_wave/utils/DHIXt.m
+++ SEIR_2_wave/utils/DHIXt.m
@@ -26,46 +26,43 @@ if dlen
 end
 T_delay = method_params(interp1(find(~isnan(T_delay)),T_delay(find(~isnan(T_delay))),1:T)'); %#ok<FNDSB>
 
-% death: najprv time-inconsistent
-omega_y = s.omega_y;       omega_o = s.omega_o;          omega = (omega_o.*varsigma+omega_y)./(1+varsigma); 
-T_death_y = s.T_death_y; % 1+1/0.1629;        
-T_death_o = s.T_death_o; % 1+1/0.1092;
-k_death = s.k_death; x_death = 1:k_death;
-po = 1./(T_death_o.*varsigma); py = 1./(T_death_y.*(1-varsigma));
-p_T_death = repmat(po.*py./(po-py),1,k_death).*(exp(-py*x_death)-exp(-po*x_death));
-p_T_death = p_T_death./sum(p_T_death,2);
-% hospitalizations
-% old-young share in hospitals
-zeta0 = (varsigma)./(1-varsigma).*omega_y./omega_o; zeta = zeta0./(1+zeta0);
-% recovery in hospitals
-T_rec_y = 7.9625;       T_rec_o = 11.7325;              T_rec = zeta.*T_rec_o+(1-zeta).*T_rec_y;
-T_rec_std = 0.5547;
-k_rec = s.k_rec;        x_rec = 1:k_rec;                T_rec_shape = T_rec*T_rec_std^2; T_rec_scale = 1/T_rec_std^2;
-p_T_rec = pdf(s.T_rec_pdf_type,repmat(x_rec,length(zeta),1),repmat(T_rec_shape,1,k_rec),repmat(T_rec_scale,length(zeta),k_rec));
-p_T_rec = p_T_rec./sum(p_T_rec,2);
-% hospital admission
-lambda_y = s.eta_y;    lambda_o = s.eta_o;      
-theta0 = zeta0.*lambda_y./lambda_o; theta = theta0./(1+theta0); lambda = theta.*lambda_o+(1-theta).*lambda_y; 
-k_hosp = s.k_hosp;     x_hosp = 1:k_hosp;  
-py = 0.9425;           
-pdf_T_hosp_y = -1./log(1-py).*py.^x_hosp./x_hosp; 
-pdf_T_hosp_y = pdf_T_hosp_y./sum(pdf_T_hosp_y,2);
-po = 0.8825;           pdf_T_hosp_o = -1./log(1-po).*po.^x_hosp./x_hosp; pdf_T_hosp_o = pdf_T_hosp_o./sum(pdf_T_hosp_o,2);
-pdf_T_hosp = theta.*pdf_T_hosp_o+(1-theta).*pdf_T_hosp_y;  p_T_hosp = pdf_T_hosp./sum(pdf_T_hosp,2);
-% T_hosp_y = s.T_hosp_y;        
-% T_hosp_o = s.T_hosp_o;                   
-% po = 1./(T_hosp_o(1).*theta); py = 1./(T_hosp_y(1).*(1-theta));
-% p_T_hosp = repmat(po.*py./(po-py),1,k_hosp).*(exp(-py*x_hosp)-exp(-po*x_hosp));
-% p_T_hosp = p_T_hosp./sum(p_T_hosp,2);
-% infections
+% death
+k_death = s.k_death;
+omega_y = s.omega_y;         pdf_d_y = repmat(s.pdf_d_y,length(varsigma),1);
+omega_o = s.omega_o;         pdf_d_o = repmat(s.pdf_d_o,length(varsigma),1);
+rho = repmat(varsigma./(1+varsigma),1,k_death);
+pdf_d = omega_o*rho.*pdf_d_o+omega_y.*(1-rho).*pdf_d_y;
+omega = sum(pdf_d,2);        pdf_d = pdf_d./omega;                 time_d = s.time_d;
+weight_d = pdf_d./time_d;
+% recovery
+k_rec = s.k_rec;
+zeta0 = rho.*omega_y./omega_o; zeta = zeta0./(1+zeta0);
+pdf_r_y = repmat(s.pdf_r_y,length(varsigma),1);
+pdf_r_o = repmat(s.pdf_r_o,length(varsigma),1);
+pdf_r = (1-omega_o)*zeta.*pdf_r_o+(1-omega_y).*(1-zeta).*pdf_r_y;
+omega_r = sum(pdf_r,2);        pdf_r = pdf_r./omega_r;             time_r = s.time_r;
+weight_r = pdf_r./time_r;
+% entry
+k_hosp = s.k_hosp;
+eta_y = s.eta_y;        eta_o = s.eta_o;
+theta0 = zeta0.*eta_y./eta_o; theta = theta0./(1+theta0); 
+pdf_h_y = repmat(s.pdf_h_y,length(varsigma),1);
+pdf_h_o = repmat(s.pdf_h_o,length(varsigma),1);
+pdf_h = eta_o.*theta.*pdf_h_o+eta_y.*(1-theta).*pdf_h_y;
+eta = sum(pdf_h,2);        pdf_h = pdf_h./eta;             time_h = s.time_h;
+weight_h = pdf_h./time_h;
 % recovery from sickness
 T_delay = extend(T_delay,length(theta)-length(T_delay));
-T_sick_y = s.T_sick_y;  T_sick_o = s.T_sick_o;   T_sick_std = s.T_sick_std; 
-T_sick = theta.*T_sick_o+(1-theta).*T_sick_y-T_delay-0*T_test_to_result;
-k_sick = s.k_sick;      x_sick = 1:k_sick;       T_sick_shape = T_sick*T_sick_std^2; T_sick_scale = 1/T_sick_std^2;
-eta = 1-lambda;
-p_T_sick = pdf(s.T_sick_pdf_type,repmat(x_sick,length(zeta),1),repmat(T_sick_shape,1,k_sick),repmat(T_sick_scale,length(zeta),k_sick));
-p_T_sick = p_T_sick./sum(p_T_sick,2);
+k_sick = s.k_sick; time_s = s.time_s;
+pdf_s_y = repmat(s.pdf_s_y,length(varsigma),1);
+pdf_s_o = repmat(s.pdf_s_o,length(varsigma),1);
+pdf_s_y = pdf('Gamma',time_s,repmat((s.T_sick_y-s.T_delay)*s.T_sick_std^2,1,k_sick),1/s.T_sick_std^2+zeros(length(T_delay,k_sick))); 
+pdf_s_y = pdf_s_y(2:s.k_sick+1)./sum(pdf_s_y(2:s.k_sick+1));
+pdf_s_o = pdf('Gamma',time_s,repmat((s.T_sick_o-s.T_delay)*s.T_sick_std^2,1,k_sick),1/s.T_sick_std^2+zeros(length(T_delay,k_sick))); 
+pdf_s_o = pdf_s_o(2:s.k_sick+1)./sum(pdf_s_o(2:s.k_sick+1));
+pdf_s = (1-eta_o).*theta.*pdf_s_o+(1-eta_y).*(1-theta).*pdf_s_y;
+pdf_s = pdf_s./sum(pdf_s,2);             
+weight_s = pdf_s./time_s;
 
 %% time shift (death->illness)
 ks = s.t_shift_clin;        xs = 1:ks;
