7f3520c5e862ab4ef29a9b7c0d8ae6de192723fb
zuzancek <zuzana.mucka@gmail.com>
2021-02-12 21:56:16 +0100

    ;j;okl;kl;k;lk

--- SEIR_2_wave/a_00_run_statistics.m
+++ SEIR_2_wave/a_00_run_statistics.m
@@ -1,13 +1,22 @@
 %% cleanup
 initialize;
 
+run_hosp = false;
+
 %% definitions
-filename_in = 'data/calibration/hosp_data.xlsx';
-filename_out = 'results/optimal_fit.mat';
+if run_hosp
+    filename_in = 'data/calibration/hosp_data.xlsx';
+    filename_out = 'results/optimal_fit_hosp.mat';
+    filename_mat = 'data/calibration/hosp_data_raw.mat';
+else
+    filename_in = 'data/calibration/icu_data.xlsx';
+    filename_out = 'results/optimal_fit_icu.mat';
+    filename_mat = 'data/calibration/icu_data_raw.mat';
+end
 
 %% data processing & distributions estimation
 tb = readtable(filename_in);
-save('data/calibration/hosp_data_raw.mat','tb');
+save(filename_mat,'tb');
 s = run_clinical_inputs_statistics(tb); 
 
 %% saving stuff
