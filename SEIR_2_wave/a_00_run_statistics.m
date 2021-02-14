2ed866af512678272e91de1a8712e841ae8cf462
zuzancek <zuzana.mucka@gmail.com>
2021-02-14 01:20:41 +0100

    ;l

--- SEIR_2_wave/a_00_run_statistics.m
+++ SEIR_2_wave/a_00_run_statistics.m
@@ -2,17 +2,16 @@
 initialize;
 
 run_hosp = false;
-out_db = cell(2,1);
+out_db = cell(3,1);
+filename_out = 'results/optimal_fit.mat';
 
 %% definitions
 for i=1:2
     if i==1
         filename_in = 'data/calibration/hosp_data.xlsx';
-        filename_out = 'results/optimal_fit_hosp.mat';
         filename_mat = 'data/calibration/hosp_data_raw.mat';
     else
         filename_in = 'data/calibration/icu_data.xlsx';
-        filename_out = 'results/optimal_fit_icu.mat';
         filename_mat = 'data/calibration/icu_data_raw.mat';
     end
 
@@ -22,10 +21,11 @@ for i=1:2
     s = run_clinical_inputs_statistics(tb); 
 
     %% saving stuff
-    save(filename_out,'s');
     out_db{i} = s;
 end
 
 par = setparam();
-s = run_clinical_statistics_mild(out_db,par);
-
+%  calculate remaining statistics
+db = run_clinical_statistics_mild(out_db,par);
+stat_total = db{1};  stat_severe = db{2}; stat_mild = db{3}; weight = par.S_H_rate;
+save(filename_out,'stat_total','stat_severe','stat_mild','weight');
--- SEIR_2_wave/utils/run_clinical_statistics_mild.m
+++ SEIR_2_wave/utils/run_clinical_statistics_mild.m
@@ -1,7 +1,32 @@
-function [s]=run_clinical_statistics_mild(db,s)
+function [db]=run_clinical_statistics_mild(db,s)
 
 db_total = db{1};
 db_severe = db{2};
+db_mild = db_total;
 ratio = s.S_H_rate;
+
+fn = fieldnames(db_total);
+n = length(fn);
+
+for i=1:n
+    pdf_total = db_total.(fn{i}).pdf;
+    pdf_severe = db_severe.(fn{i}).pdf;
+    k = max(length(pdf_total),length(pdf_severe));
+    pdf_total(end+1:k) = 0;     db_total.(fn{i}).pdf = pdf_total;
+    pdf_severe(end+1:k) = 0;    db_severe.(fn{i}).pdf = pdf_severe;
+    pdf_mild = (pdf_total-ratio.*pdf_severe)./(1-ratio);
+    alpha = (db_total.(fn{i}).alpha-ratio.*db_total.(fn{i}).alpha)./(1-ratio);
+    db_mild.(fn{i}).type = '';
+    db_mild.(fn{i}).obj = [];
+    db_mild.(fn{i}).pdf = pdf_mild;
+    db_mild.(fn{i}).cdf = cumsum(pdf_mild);
+    db_mild.(fn{i}).diff = 0;
+    db_mild.(fn{i}).time_grid = 0:k-1;
+    db_mild.(fn{i}).alpha = alpha;
+end
+
+db{1} = db_total;
+db{2} = db_severe;
+db{3} = db_mild;
 
 end
