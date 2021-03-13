1ae8f436f6a0aae766e5a4344760bf5819bc5b08
zuzancek <zuzana.mucka@gmail.com>
2021-03-13 12:35:00 +0100

    liil

--- SEIR_2_wave/Matlab/aa.m
+++ SEIR_2_wave/Matlab/aa.m
@@ -47,13 +47,13 @@ N_y = s.pop_size_y;
 N_s = 8;
 N_t = 10;
 % *** matrices
-% 
+%
 % Q_mat: transition times between states, size: N_t x N, different for o and y
 Q0_mat_y = zeros(N_t,N_y); Q0_mat_o = zeros(N_t,N_o);
 Q_mat_y = zeros(N_t,N_y); Q_mat_o = zeros(N_t,N_o);
 Q_mat_y_next = Q_mat_y; Q_mat_o_next = Q_mat_o;
 create_Q_matrix();
-% 
+%
 % S_mat: current state for each agent, size N_s x N, different for o and y
 % St_mat: time-evolution of states, aggregated, size T_total x N_s
 S_mat_y = []; S_mat_o = []; SR_mat_y = []; SR_mat_o = []; St_mat_y=[]; St_mat_o=[]; St_mat=[];
@@ -61,12 +61,15 @@ S_mat_y_next = S_mat_y; S_mat_o_next = S_mat_o_next;
 create_S_matrix();
 %
 % P_mat: state-transition probabilities, size N_s x N_s, different for o and y
+P_mat_y = zeros(N_s,N_s);
 create_P_matrix();
 %
 % ST_mat: state-transition mapping, size N_s x N_s
 % TS_mat: transition-state mapping, size N_t x 2
 % U_mat: U-distr. random _vectors for transition probabilities in nodes with
 % multiple out-paths, size 3 x N, different for o and y
+U_mat_y = rand([3,N_y]);
+U_mat_o = rand([3,N_o]);
 create_aux_matrices();
 
 %%
@@ -77,8 +80,8 @@ T_inf_vec = random(s.obj_inf,1,N);
 betta_vec = Rt_vec./T_inf_vec;
 profile_U_y = []; profile_I_y = []; profile_U_o = []; profile_I_o = [];
 SE_y_vec = zeros(1,N_y); RiS_y_vec = SE_y_vec; RhS_y_vec = SE_y_vec; EU_y_vec = SE_y_vec;
-URi_y_vec = SE_y_vec; IRi_y_vec = SE_y_vec; HRh_y_vec = SE_y_vec;
-SE_o_vec = zeros(1,N_o); RiS_o_vec = SE_o_vec; RhS_o_vec = SE_o_vec; 
+URi_y_vec = SE_y_vec; IRi_y_vec = SE_y_vec; HRh_y_vec = SE_y_vec; UI_y_vec = SE_y_vec;
+SE_o_vec = zeros(1,N_o); RiS_o_vec = SE_o_vec; RhS_o_vec = SE_o_vec; UI_o_vec = SE_o_vec;
 URi_o_vec = SE_o_vec; IRi_o_vec = SE_o_vec; HRh_o_vec = SE_o_vec; EU_o_vec = SE_o_vec;
 get_inf_profile();
 
@@ -90,13 +93,14 @@ Ih_y_vec = zeros(1,N_y); Ih_o_vec = zeros(1,N_o); % immunity (after infection)
 
 for t=1:T_total
     store_states_transitions();
-    update_vaccination_immunity(); % vymysliet
+    update_vaccination_immunity(); % todo: vymysliet vakcinaciu
     update_suspectible();
     update_exposed();
+    update_unobserved();
+    
     
     update_removed();
-    Q0_mat_y(1,SE_y_vec)=0;
-    Q0_mat_o(1,SE_o_vec)=0;
+    upddate_states_transitions(); % todo
 end
 
     function []=store_states_transitions()
@@ -126,14 +130,14 @@ end
         % calculate outflow of suspectible individuals (=inflow of exposed)
         calculate_E_inflow();
         % update transition matrices
-        Q_mat_y_next(1,:) = Q_mat_y(1,:)-1;        
-        Q_mat_o_next(1,:) = Q_mat_o(1,:)-1;       
+        Q_mat_y_next(1,:) = Q_mat_y(1,:)-1;
+        Q_mat_o_next(1,:) = Q_mat_o(1,:)-1;
         % newly suspectible time spent in this state is set to 1 and those
         % who are exposed have 0
-        Q_mat_y_next(1,RiS_y_vec+RhS_y_vec) = T_total;   
-        Q_mat_o_next(1,RiS_o_vec+RhS_o_vec) = T_total;     
-        Q_mat_y_next(1,SE_y_vec) = 0;   
-        Q_mat_o_next(1,SE_o_vec) = 0;           
+        Q_mat_y_next(1,RiS_y_vec+RhS_y_vec) = T_total;
+        Q_mat_o_next(1,RiS_o_vec+RhS_o_vec) = T_total;
+        Q_mat_y_next(1,SE_y_vec) = 0;
+        Q_mat_o_next(1,SE_o_vec) = 0;
         % update state
         S_mat_y_next(1,:) = S_mat_y_next(1,:)-SE_y_vec+RiS_y_vec+RhS_y_vec; % outflow of suspectible, new exposed
         S_mat_o_next(1,:) = S_mat_o_next(1,:)-SE_o_vec+RiS_o_vec+RhS_o_vec; % outflow of suspectible, new exposed
@@ -143,46 +147,71 @@ end
         % transitions: in 1 (SE), out 2 (EU)
         % state: 2
         % decrement time elapsed
-        Q_mat_y_next(2,:) = Q_mat_y(2,:)-1;        
-        Q_mat_o_next(2,:) = Q_mat_o(2,:)-1;    
+        Q_mat_y_next(2,:) = Q_mat_y(2,:)-1;
+        Q_mat_o_next(2,:) = Q_mat_o(2,:)-1;
         % determine agents leaving current state
         EU_y_vec = (Q_mat_y_next(2,:)==0);
         EU_o_vec = (Q_mat_o_next(2,:)==0);
         % newly exposed time spent in this state is set to their
         % corresponding def.values
         Q_mat_y_next(2,EU_y_vec) = Q0_mat_y(2,EU_y_vec);
-        Q_mat_o_next(2,EU_o_vec) = Q0_mat_o(2,EU_o_vec);  
+        Q_mat_o_next(2,EU_o_vec) = Q0_mat_o(2,EU_o_vec);
         % update state
         S_mat_y_next(2,:) = S_mat_y_next(2,:)+SE_y_vec-EU_y_vec;
-        S_mat_o_next(2,:) = S_mat_o_next(2,:)+SE_o_vec-EU_o_vec;    
+        S_mat_o_next(2,:) = S_mat_o_next(2,:)+SE_o_vec-EU_o_vec;
+    end
+
+    function []=update_unobserved()
+        % transitions: in 2 (EU); out 3 (UI), 4 (URi)
+        % state: 3
+        % decrement time elapsed
+        Q_mat_y_next(3,:) = Q_mat_y(3,:)-1;  Q_mat_y_next(4,:) = Q_mat_y(4,:)-1;
+        Q_mat_o_next(3,:) = Q_mat_o(3,:)-1;  Q_mat_o_next(4,:) = Q_mat_o(4,:)-1;
+        % determine agents leaving current state
+        UI_y_vec = (Q_mat_y_next(3,:)==0);   URi_y_vec = (Q_mat_y_next(4,:)==0);
+        UI_o_vec = (Q_mat_o_next(3,:)==0);   URi_o_vec = (Q_mat_o_next(4,:)==0);
+        % newly infectious but yet unobserved (not tested/reported) time spent in this state is set to their
+        % corresponding def.values
+        % determine observed agents
+        [~,ui_new_y_idx] = maxk(U_mat_y(1,:), ceil(P_mat_y(3,4)*sum(EU_y_vec)));
+        [~,ui_new_o_idx] = maxk(U_mat_o(1,:), ceil(P_mat_o(3,4)*sum(EU_o_vec)));
+        Q_mat_y_next(3,ui_new_y_idx) = Q0_mat_y(3,ui_new_y_idx);
+        Q_mat_o_next(3,ui_new_o_idx) = Q0_mat_o(3,ui_new_o_idx);
+        Q_mat_y_next(4,ui_new_y_idx) = Q0_mat_y(4,ui_new_y_idx);
+        Q_mat_o_next(4,ui_new_o_idx) = Q0_mat_o(4,ui_new_o_idx);
+        % update state
+        S_mat_y_next(3,:) = S_mat_y_next(3,:)+EU_y_vec-UI_y_vec-URi_y_vec;
+        S_mat_o_next(3,:) = S_mat_o_next(3,:)+EU_o_vec-UI_o_vec-URi_o_vec;
     end
 
     function []=update_removed()
         % temporarily removed
+        % transitions: in ; out 9 (RiS) and 10 (RhS)
+        % states:  7 (Ri), 8 (Rh)
         % decrement time elapsed
         Q_mat_y_next(9,:) = Q_mat_y(9,:)-1;        Q_mat_y_next(10,:) = Q_mat_y(10,:)-1;
         Q_mat_o_next(9,:) = Q_mat_o(9,:)-1;        Q_mat_o_next(10,:) = Q_mat_o(10,:)-1;
         % newly removed time spent in this state is set to their
         % corresponding def.values
-        Q_mat_y_next(9,URi_y_vec) = Q0_mat_y(9,URi_y_vec);   
-        Q_mat_y_next(10,HRh_y_vec) = Q0_mat_y(10,HRh_y_vec);        
+        Q_mat_y_next(9,URi_y_vec) = Q0_mat_y(9,URi_y_vec);
+        Q_mat_y_next(10,HRh_y_vec) = Q0_mat_y(10,HRh_y_vec);
         Q_mat_y_next(9,IRi_y_vec) = Q0_mat_y(9,IRi_y_vec);
-        Q_mat_o_next(9,URi_o_vec) = Q0_mat_o(9,URi_o_vec);   
-        Q_mat_o_next(10,HRh_o_vec) = Q0_mat_o(10,HRh_o_vec);        
+        Q_mat_o_next(9,URi_o_vec) = Q0_mat_o(9,URi_o_vec);
+        Q_mat_o_next(10,HRh_o_vec) = Q0_mat_o(10,HRh_o_vec);
         Q_mat_o_next(9,IRi_o_vec) = Q0_mat_o(9,IRi_o_vec);
         % increase default removal time for those already temporarily
         % removed (better immunity)
-        Q0_mat_y(9,URi_y_vec) = round(1.5*Q0_mat_y(9,URi_y_vec)); 
+        Q0_mat_y(9,URi_y_vec) = round(1.5*Q0_mat_y(9,URi_y_vec));
         Q0_mat_o(9,URi_o_vec) = round(1.5*Q0_mat_o(9,URi_o_vec));
-        Q0_mat_y(9,iRi_y_vec) = round(1.5*Q0_mat_y(9,IRi_y_vec)); 
+        Q0_mat_y(9,iRi_y_vec) = round(1.5*Q0_mat_y(9,IRi_y_vec));
         Q0_mat_o(9,IRi_o_vec) = round(1.5*Q0_mat_o(9,IRi_o_vec));
-        Q0_mat_o(10,HRh_y_vec) = round(1.5*Q0_mat_o(10,HRh_y_vec)); 
-        Q0_mat_o(10,HRh_o_vec) = round(1.5*Q0_mat_o(10,HRh_o_vec));        
+        Q0_mat_o(10,HRh_y_vec) = round(1.5*Q0_mat_o(10,HRh_y_vec));
+        Q0_mat_o(10,HRh_o_vec) = round(1.5*Q0_mat_o(10,HRh_o_vec));
         % newly suspectible (= outflow) - after loosing immunity
         RiS_y_vec = (Q_mat_y_next(9,:)==0);
         RhS_y_vec = (Q_mat_y_next(10,:)==0);
         RiS_o_vec = (Q_mat_o_next(9,:)==0);
-        RhS_o_vec = (Q_mat_o_next(10,:)==0);  
+        RhS_o_vec = (Q_mat_o_next(10,:)==0);
         % update state (+inflow -outflow)
         S_mat_y_next(7,:) = S_mat_y_next(7,:)+URi_y_vec+IRi_y_vec-RiS_y_vec;
         S_mat_y_next(8,:) = S_mat_y_next(8,:)+HRh_y_vec-RhS_y_vec;
@@ -204,7 +233,7 @@ end
         [~,SE_y_idx] = maxk(Wy_vec,ky);
         [~,SE_o_idx] = maxk(Wo_vec,ko);
         SE_y_vec = 0*SE_y_vec; SE_y_vec(SE_y_idx) = 1;
-        SE_o_vec = 0*SE_o_vec; SE_o_vec(SE_o_idx) = 1;        
+        SE_o_vec = 0*SE_o_vec; SE_o_vec(SE_o_idx) = 1;
     end
 
     function []=update_vaccination_immunity()
@@ -217,7 +246,7 @@ end
     end
 
     function []=create_Q_matrix()
-        % transition time matrix (max per agent)      
+        % transition time matrix (max per agent)
         Q0_mat_y(1,:) = T_total;                                   % SE
         Q0_mat_y(2,:) = round(random(s.obj_lat,1,N_y));            % EU
         Q0_mat_y(3,:) = round(random(s.obj_pre_test,1,N_y));       % UI
@@ -241,7 +270,7 @@ end
     end
 
     function []=create_S_matrix() %%  add init data here !!!!
-        S_mat_y = zeros(N_s,N_y);        
+        S_mat_y = zeros(N_s,N_y);
         S_mat_y(1,randperm(N_y,S0_y)) = 1;
         S_mat_y(2,randperm(N_y,E0_y)) = 1;
         S_mat_y(3,randperm(N_y,U0_y)) = 1;
@@ -250,7 +279,7 @@ end
         S_mat_y(6,randperm(N_y,D0_y)) = 1;
         S_mat_y(7,randperm(N_y,Ri0_y)) = 1;
         S_mat_y(8,randperm(N_y,Rh0_y)) = 1;
-        S_mat_o = zeros(N_s,N_y);        
+        S_mat_o = zeros(N_s,N_y);
         S_mat_o(1,randperm(N_y,S0-S0_y)) = 1;
         S_mat_o(2,randperm(N_y,E0-E0_y)) = 1;
         S_mat_o(3,randperm(N_y,U0-U0_y)) = 1;
@@ -269,7 +298,7 @@ end
     end
 
     function []=create_P_matrix()
-        P_mat_y = zeros(N_s,N_s); % S E U I H D Ri Rh
+        % S E U I H D Ri Rh
         P_mat_y(1,2) = 1; P_mat_y(2,3) = 1; P_mat_y(7,1) = 1; P_mat_y(8,1) = 1; P_mat_o = P_mat_y;
         P_mat_y(3,4) = init.obs_y_ss; P_mat_y(3,7) = 1-init.obs_y_ss;
         P_mat_y(4,5) = init.eta_y_ss; P_mat_y(4,7) = 1-init.eta_y_ss;
@@ -282,11 +311,9 @@ end
     end
 
     function []=create_aux_matrices()
-        U_mat_y = rand([3,N_y]);
-        U_mat_o = rand([3,N_o]);  
         ST_mat = zeros(N_s,N_s);
         ST_mat(1,2)=1;        ST_mat(2,3)=2;        ST_mat(3,4)=3;         ST_mat(3,7)=4;         ST_mat(4,5)=5;
-        ST_mat(4,7)=6;        ST_mat(5,6)=7;        ST_mat(5,8)=8;         ST_mat(7,1)=9;         ST_mat(8,1)=10;   
+        ST_mat(4,7)=6;        ST_mat(5,6)=7;        ST_mat(5,8)=8;         ST_mat(7,1)=9;         ST_mat(8,1)=10;
         TS_mat = zeros(N_t,2);
         TS_mat(1,1) = 1;       TS_mat(1,2) = 2;
         TS_mat(2,1) = 2;       TS_mat(2,2) = 3;
@@ -297,19 +324,19 @@ end
         TS_mat(7,1) = 5;       TS_mat(7,2) = 6;
         TS_mat(8,1) = 5;       TS_mat(8,2) = 8;
         TS_mat(9,1) = 7;       TS_mat(9,2) = 1;
-        TS_mat(10,1) = 8;      TS_mat(10,2) = 1;                
+        TS_mat(10,1) = 8;      TS_mat(10,2) = 1;
         NT_mat = [3 5 7;4 6 8]; % p, 1-p
-    end 
+    end
 
     function []=get_inf_profile()
         mu = max(Q0_mat_y(4,:));
-        profile_U_y = ones(ceil(mu),N_y);        
+        profile_U_y = ones(ceil(mu),N_y);
         mi = max(Q0_mat_y(6,:));
-        profile_I_y = ones(ceil(mi),N_y);    
+        profile_I_y = ones(ceil(mi),N_y);
         mu = max(Q0_mat_o(4,:));
-        profile_U_o = ones(ceil(mu),N_o);        
+        profile_U_o = ones(ceil(mu),N_o);
         mi = max(Q0_mat_o(6,:));
-        profile_I_o = ones(ceil(mi),N_o);       
+        profile_I_o = ones(ceil(mi),N_o);
     end
-   
+
 end
