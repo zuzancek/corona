a87968465ae339b9b598e6110a57f85765265f73
zuzancek <zuzana.mucka@gmail.com>
2021-03-18 21:29:42 +0100

    kjklj

--- SEIR_2_wave/Matlab/plotting/check_targets.m
+++ SEIR_2_wave/Matlab/plotting/check_targets.m
@@ -1,3 +1,33 @@
 function []=check_targets(varargin)
 
+ip = inputParser;
+addParamValue(ip, 'dbs', {}, @iscell);%#ok<*NVREPL>
+addParamValue(ip, 'varlist',{}, @iscell);
+addParamValue(ip, 'titles',{}, @iscell);
+addParamValue(ip, 'legend',{}, @iscell);
+addParamValue(ip, 'dateFrom',dd(2020,9,1), @isnumeric);
+addParamValue(ip, 'dateTo',dd(2020,12,31), @isnumeric);
+parse(ip, varargin{:});
+results = ip.Results;
 
+dateFrom = results.dateFrom;
+dateTo = results.dateTo;
+tit = results.titles;
+dbs = results.dbs;
+varlist = results.varlist;
+leg = results.legend;
+n = length(dbs);
+m = length(varlist);
+
+for i=1:m
+    vars = varlist{i};
+    figure;
+    for j=1:n
+    	plot(resize(dbs{j}.(vars{j}),dateFrom:dateTo),'linewidth',1); hold on;
+    end
+    grid on;
+    legend(leg);
+    title(tit{i});
+end
+    
+end
